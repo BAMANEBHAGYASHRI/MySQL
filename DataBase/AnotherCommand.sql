cc7fbafc231f752045b5e6ce072a57aaabf2433d
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-09-30 22:49:29 +0530

    Database Commands-SELECT ,USE ,SHOW DATABASE

---
+++ DataBase/AnotherCommand.sql
@@ -0,0 +1,10 @@
+-- seen all the that it does is list the current databases;
+SHOW DATABASES;
+
+-- The use command is used when there are multiple databases in the SQL and the user or programmer specifically wants to use a particular database
+USE Database_name;
+
+-- SELECT Database is used in MySQL to select a particular database to work with.
+SELECT DATABASE();
+
+
