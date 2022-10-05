eee0db1847f9d3a6eee5612ca5c63d266b5d1da9
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-10-05 22:17:15 +0530

    challange time

---
+++ Tables/Challange1.sql
@@ -0,0 +1,12 @@
+CREATE TABLE people(
+    fname VARCHAR(20),
+    lname VARCHAR(20),
+    age INT
+);
+
+INSERT INTO people(fname,lname,age)
+    VALUES("tina","belchor",13),("bob","belchor",18);
+
+
+SELECT * FROM people;
+
